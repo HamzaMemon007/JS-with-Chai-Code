1a5c3449d90185d233aadec0ba5545659aaca576
HamzaMemon007 <162428814+HamzaMemon007@users.noreply.github.com>
2025-01-04 00:32:02 +0500

    problem 2

--- Array-Practice/Index.js
+++ Array-Practice/Index.js
@@ -1,4 +1,15 @@
+// problem no 1
 let teaTypes = ["herbel tea","white tea","masala chai"]
 
 teaTypes[1] = "jasmine tea"
-console.log(teaTypes);
+// console.log(teaTypes);
+
+// problem no 2
+
+let topCities = ["Berlin", "Singapore", "New York"];
+let hardCopyCities = [...topCities];
+
+topCities.pop();
+console.log(topCities);
+console.log(hardCopyCities);
+
