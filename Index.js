dbcc40f04a27e9f23287cdaf4b56e38c55e13dc7
HamzaMemon007 <162428814+HamzaMemon007@users.noreply.github.com>
2025-01-05 21:37:49 +0500

    problem 1 loop

---
+++ Index.js
@@ -0,0 +1,50 @@
+// // problem no 1
+// let teaTypes = ["herbel tea","white tea","masala chai"]
+
+// teaTypes[1] = "jasmine tea"
+// // console.log(teaTypes);
+
+// // problem no 2
+
+// let topCities = ["Berlin", "Singapore", "New York"];
+// let hardCopyCities = [...topCities];
+
+// topCities.pop();
+// // console.log(topCities);
+// // console.log(hardCopyCities);
+
+// // Problem 3
+
+// let popularTeas = ["oolong tea","green tea"]
+// let softCopyTeas = popularTeas;
+// popularTeas.pop();
+// // console.log(popularTeas);
+// // console.log(softCopyTeas);
+
+// // Problem 4
+// let europeanCities = ["Paris", "Rome"];
+// let asianCities = ["Tokyo", "Bangkok"];
+// let worldCities = europeanCities.concat(asianCities);
+// // console.log(worldCities);
+
+// // Problem 5
+
+// let cityBucketList = ["Kyoto", "London", "Cape Town", "Vancouver"];
+// let isLondonInList = cityBucketList.includes("London");
+
+
+// Loops starts from here
+
+// problem 1
+
+
+let sum = 0;
+let i = 1
+
+while (i <=5) {
+    sum = sum + i;
+    i++;
+}
+//  console.log(sum);
+
+
